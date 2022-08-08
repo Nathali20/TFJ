eb39045726a7c9f883f9b251c093885f6cd84190
Nathalie <102970749+Nathali20@users.noreply.github.com>
2022-08-08 19:10:19 +0200

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/repository/CategorieRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+
+import org.springFramework.data.jpa.repository.JpaRepository;
+import com.example.demo.modele.Categorie;
+
+public interface CategorieRepository extends JpaRepository<Categorie, Long> {
+}
---
+++ demo/src/main/java/com/example/demo/repository/MaterielRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+import com.example.demo.modele.Materiel;
+
+import org.springFramework.data.jpa.repository.JpaRepository;
+
+public interface MaterielRepository extends JpaRepository<Materiel, Long>{
+}
